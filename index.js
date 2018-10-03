0a301a0a0d040fb0e0c81ac57264dbeacb6130b0
severus889700 <severus889700@gmail.com>
2018-10-03 22:08:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function currentLine (katzDeliLine){
     return "The line is currently empty."
   }
    var myString = `The line is currently: 1. ${katzDeliLine[0]}`
-   for(let i = 1; i<katzDeliLine.length;i++){
+   for(var i = 1; i<katzDeliLine.length;i++){
   myString += `, ${i+1}. ${katzDeliLine[i]}`
 }
 return myString
